4cc302b94e1cdeaeb6c47cc22e06742b3edc1159
Martinic <info@martinic.com>
2023-01-27 10:06:16 +0100

    Added preliminary source code, thnx ChatGPT!

---
+++ DrMixAISynth.h
@@ -0,0 +1,43 @@
+#include "WDL/wdltypes.h"
+#include "WDL/ptrlist.h"
+#include "WDL/synth.h"
+
+class SineSynth : public WDL_Synth
+{
+public:
+  SineSynth() : m_frequency(0), m_phase(0) {}
+
+  void SetFrequency(double frequency) { m_frequency = frequency; }
+
+  void Process(double *output, int samples)
+  {
+    double phase_step = m_frequency / GetSampleRate();
+
+    for (int i = 0; i < samples; i++)
+    {
+      output[i] = sin(m_phase);
+      m_phase += phase_step;
+    }
+  }
+
+private:
+  double m_frequency;
+  double m_phase;
+};
+
+class MySynth : public WDL_Synth
+{
+public:
+  MySynth() : m_sine(new SineSynth()) {}
+  ~MySynth() { delete m_sine; }
+
+  void SetFrequency(double frequency) { m_sine->SetFrequency(frequency); }
+
+  void Process(double *output, int samples)
+  {
+    m_sine->Process(output, samples);
+  }
+
+private:
+  SineSynth *m_sine;
+};
